2e0b5538c27ac0c6931886489b0ae41f8a2cf6c0
Mateus Silva <mateus42silva@gmail.com>
2025-06-27 11:30:32 -0300

    add redux to project

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import  {useSelector, useDispatch} from 'react-redux'
+import UserActionTypes from '../../redux/user/action-types';
+import { loginUser, logoutUser } from "../../redux/user/actions";
 
 // Components
 import Cart from "../cart/index";
@@ -9,15 +12,36 @@ import * as Styles from "./styles";
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
+  const {currentUser} = useSelector((rootReducer) => rootReducer.userReducer);
+  const dispatch = useDispatch();
+
+  console.log('Current user = ', currentUser)
+
   const handleCartClick = () => {
     setCartIsVisible(true);
   };
+
+  const handleLoginClick = () => {
+    dispatch(loginUser({
+      name: "Mateus S",
+      email: "mateus@email.com"
+    }))
+  }
+
+  const handleLogoutClick = () => {
+    dispatch(logoutUser())
+  }
 
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+        {currentUser ? (
+          <div onClick={handleLogoutClick}>Sair</div>
+          ) : ( 
+            <div onClick={handleLoginClick}>Login</div>
+          )}
+       
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
 
